3d9613e56d2600ce706fbd940fc601dc39462e6a
User <User@DESKTOP-VI7HLAA>
2023-08-31 19:09:46 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Data.SqlClient;
+
+
+namespace L2
+{
+    internal class Program
+    {
+        private static string ConString => "Data Source=DESKTOP-VI7HLAA\\SQLEXPRESS;Initial Catalog=BarberShop;Integrated Security=True;Connect Timeout=30;";
+        static void Main(string[] args)
+        {
+            using(var SqlCon = new SqlConnection(ConString))
+            {
+                SqlCon.Open();
+
+                SqlCon.Close();
+            }
+        }
+    }
+}
